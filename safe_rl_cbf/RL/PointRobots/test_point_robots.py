ba6783f78395814c1947e3552463978ee6c20d51
sunnywang <X.WANG-55@student.tudelft.nl>
2023-08-02 10:05:10 +0200

    point robots coordinates

--- safe_rl_cbf/RL/PointRobots/test_point_robots.py
+++ safe_rl_cbf/RL/PointRobots/test_point_robots.py
@@ -2,34 +2,32 @@ import numpy as np
 from stable_baselines3 import PPO
 import torch
 
-from safe_rl_cbf.RL.PointRobot.point_robot_env import PointRobotEnv
+from safe_rl_cbf.RL.PointRobots.point_robots_env import PointRobotsEnv
 from safe_rl_cbf.NeuralCBF.MyNeuralNetwork import NeuralNetwork
-from safe_rl_cbf.Dynamics.dynamic_system_instances import dubins_car, point_robot
+from safe_rl_cbf.Dynamics.dynamic_system_instances import dubins_car, point_robot, point_robots_dis
 from safe_rl_cbf.Dataset.TrainingDataModule import TrainingDataModule
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-system = point_robot
+system = point_robots_dis
 render_sim = True #if True, a graphic is generated
 
-env = PointRobotEnv(render_sim=render_sim)
+env = PointRobotsEnv(render_sim=render_sim)
 
 # model = PPO.load("new_agent")
 # model.set_env(env)
 
 data_module = TrainingDataModule(system=system, val_split=0, train_batch_size=512, training_points_num=int(1e6))
-NN = NeuralNetwork.load_from_checkpoint("saved_models/point_robot_dist/checkpoints/epoch=356-step=58191.ckpt", dynamic_system=system, data_module=data_module )
+NN = NeuralNetwork.load_from_checkpoint("saved_models/point_robots/checkpoints/epoch=91-step=14996.ckpt", dynamic_system=system, data_module=data_module )
 NN.to(device)
 
 env.set_barrier_function(NN)
 
 obs = env.reset()
 
-random_action = np.random.rand(2) - 0.5 
-print("random_action", random_action)
 try:
     while True:
         # action, _ = model.predict(obs) 
-        action = random_action
+        action = np.random.rand(2) * 0.5 - 0.25 
         obs, reward, done, info = env.step(action)
         if render_sim is True:
             env.render()
@@ -37,7 +35,7 @@ try:
             obs = env.reset()
         if env.current_time_step > 700:
 
-            random_action = np.random.rand(2) - 0.5
+            random_action = np.random.rand(2) * 0.5 - 0.25
             print("random_action", random_action)
             print("Time out")
             obs = env.reset()
