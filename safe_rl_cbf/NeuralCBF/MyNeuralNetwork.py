b84d11e2bee305d96d50fc3bce21fc08e24c479a
sunnywang <780178183@qq.com>
2023-08-02 06:40:04 +0000

    train point robots without constraints

--- safe_rl_cbf/NeuralCBF/MyNeuralNetwork.py
+++ safe_rl_cbf/NeuralCBF/MyNeuralNetwork.py
@@ -78,11 +78,11 @@ class NeuralNetwork(pl.LightningModule):
         self.fine_tune = fine_tune
         self.require_grad_descent_loss = require_grad_descent_loss
         self.h = nn.Sequential(
-                nn.Linear(self.dynamic_system.ns, 256),
+                nn.Linear(self.dynamic_system.ns, 512),
                 nn.Tanh(),
-                nn.Linear(256, 256),
+                nn.Linear(512, 512),
                 nn.Tanh(),
-                nn.Linear(256, 256),
+                nn.Linear(512, 256),
                 nn.Tanh(),
                 nn.Linear(256,1)
             )
@@ -2009,7 +2009,7 @@ class NeuralNetwork(pl.LightningModule):
                 pass
                 # self.trainer.should_stop = True
         
-            if self.training_stage == 0 and (performance_losses < 5 and safety_losses < 5): # warm up, shape h and g
+            if self.training_stage == 0 and (performance_losses < 1 and safety_losses < 1): # warm up, shape h and g
                 self.trainer.should_stop = True
                 self.training_stage = 1  # start to consider condition 3
                 self.epoch_record = self.current_epoch
